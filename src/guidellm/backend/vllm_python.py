1929d2f49ee79be93397a7a077b957f470dc015e
jaredoconnell <joconnel@redhat.com>
2025-07-03 21:15:13 +0000

    Progress towards vLLM backend

--- src/guidellm/backend/vllm_python.py
+++ src/guidellm/backend/vllm_python.py
@@ -5,13 +5,17 @@ from PIL import Image
 
 from guidellm.backend import StreamingTextResponse, ResponseSummary
 from guidellm.backend.backend import Backend
+from sympy.physics.units import time
 
 try:
-    from vllm import LLM, SamplingParams
+    from vllm.engine.async_llm_engine import AsyncLLMEngine, AsyncEngineArgs, PromptType, SamplingParams, RequestOutput
     VLLM_ENABLED = True
 except ImportError:
-    LLM = None
+    AsyncLLMEngine = None
+    AsyncEngineArgs = None
+    PromptType = None
     SamplingParams = None
+    RequestOutput = None
     VLLM_ENABLED = False
 
 
@@ -23,6 +27,8 @@ class VLLMPythonBackend(Backend):
     Runs directly with Python.
     """
 
+    llm_engine: AsyncLLMEngine
+
     def __init__(
         self,
         model: Optional[str] = None,
@@ -30,6 +36,8 @@ class VLLMPythonBackend(Backend):
         super().__init__(type_="vllm_python")
 
         self._model = model
+        args = AsyncEngineArgs(model=model)
+        self.llm_engine = AsyncLLMEngine.from_engine_args(args)
 
     @property
     def model(self) -> Optional[str]:
@@ -56,13 +64,13 @@ class VLLMPythonBackend(Backend):
         pass
 
     async def available_models(self) -> list[str]:
-
+        # Do we check the cache or some other directory? ~/.cache/huggingface/hub/
         pass
 
     async def text_completions(self, prompt: Union[str, list[str]], request_id: Optional[str] = None,
                                prompt_token_count: Optional[int] = None, output_token_count: Optional[int] = None,
                                **kwargs) -> AsyncGenerator[Union[StreamingTextResponse, ResponseSummary], None]:
-        pass
+
 
     async def chat_completions(self, content: Union[
         str,
@@ -71,7 +79,56 @@ class VLLMPythonBackend(Backend):
     ], request_id: Optional[str] = None, prompt_token_count: Optional[int] = None,
                                output_token_count: Optional[int] = None, raw_content: bool = False, **kwargs) -> \
     AsyncGenerator[Union[StreamingTextResponse, ResponseSummary], None]:
-        pass
+        params = SamplingParams(
+            max_tokens=output_token_count,
+            # Set min token count?
+        )
+        start_time = time.time()
+        llm_generator = self.llm_engine.generate(PromptType.TEXT_COMPLETIONS, params, request_id)
+        return self._handle_llm_output(llm_generator, start_time)
+
+    async def _handle_llm_output(
+            self,
+            generator: AsyncGenerator[RequestOutput, None],
+            start_time: float,
+            request_id: Optional[str],
+    ):
+        response_prompt_count: Optional[int] = None
+        response_output_count: Optional[int] = None
+        iter_count = 0
+        iter_time = start_time
+        first_iter_time: Optional[float] = None
+        last_iter_time: Optional[float] = None
+
+        yield StreamingTextResponse(
+            type_="start",
+            value="",
+            start_time=start_time,
+            first_iter_time=None,
+            iter_count=iter_count,
+            delta="",
+            time=start_time,
+            request_id=request_id,
+        )
+
+        async for output in generator:
+            iter_count += 1
+            if output.finished:
+                print("Got finished output:", output)
+                yield
+            else:
+                print("Got unfinished output:", output)
+                yield StreamingTextResponse(
+                    type_="iter",
+                    value="TODO",  # TODO: Convert output.outputs
+                    iter_count=iter_count,
+                    start_time=start_time,
+                    first_iter_time=first_iter_time,
+                    delta="TODO",  # Does it give iterative changes?
+                    time=iter_time,
+                    request_id=request_id,
+                )
+
 
     @property
     def target(self) -> str:
